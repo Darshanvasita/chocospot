ff13c4642c3a3dc62d620b3f3e4ecb975348de52
DHWANIL018 <pradhwanil@gmail.com>
2025-03-27 10:59:08 +0530

    some errror are fixed

--- Frontend/src/components/NewsLetterBox.jsx
+++ Frontend/src/components/NewsLetterBox.jsx
@@ -16,7 +16,7 @@ const NewsLetterBox = () => {
         </p>
 
         <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
-            <input type="text" className=' w-full sm:fle-1 outline-none' type='email' placeholder='Enter Your email' required />
+            <input className=' w-full sm:fle-1 outline-none' type='email' placeholder='Enter Your email' required />
             <button type='submit' className='bg-black text-white text-xs px-10 py-4 '>SUBSCRIBE</button>
         </form>
 
