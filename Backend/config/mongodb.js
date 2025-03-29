7ce23ccebd0bf091724d962554ea1a2196f1e964
DHWANIL018 <pradhwanil@gmail.com>
2025-03-29 14:41:30 +0530

    refactor: update MongoDB connection URL and simplify cart logic

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -3,7 +3,7 @@
 const { default: mongoose } = require("mongoose");
 
 exports.connectDB = async ()=> {
-    await mongoose.connect(`${process.env.MONGODB_URL}e-commerce1`)
+    await mongoose.connect(`${process.env.MONGODB_URL}chocospot`)
     .then(()=>{
         console.log("DB Connected")
     }).catch((err)=>{
